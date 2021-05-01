0f4f4b1de2406a7cd8992aa9900e9722531fb5cc
Dionata Mesquita <dionata.bm@gmail.com>
2021-05-01 17:28:42 -0300

    Primeiro commit

---
+++ api-server/start/routes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Route = use('Route')
+
+//Route.get('/', () => {
+//  return { greeting: 'Hello world in JSON' }
+//})
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
